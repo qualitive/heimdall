c43a29c4c9457a7f2a79ad1786a932ffaf9de644
Elias Jørgensen <elias.zakarias.joergensen@pleo.io>
2024-02-06 21:25:14 +0100

    Initial commit - setup Ktor project

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.22"
+    application
+}
+
+group = "com.qualitive"
+version = "0.0.1"
+
+application {
+    mainClass.set("com.qualitive.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
